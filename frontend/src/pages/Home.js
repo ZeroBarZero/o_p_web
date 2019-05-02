a8f98e96d187df2300ee33227b8805da8f6e90cd
yujin100b <jjem22@naver.com>
2019-05-02 19:55:51 +0900

    react + reactstrap + redux

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>
+                홈
+            </h2>
+        </div>
+    );
+};
+
+export default Home
