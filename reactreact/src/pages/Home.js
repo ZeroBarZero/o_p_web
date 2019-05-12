dc98ee85b2a6126035bfdbff25e5d35c6699f538
yujin100b <jjem22@naver.com>
2019-05-12 23:18:06 +0900

    ..

---
+++ reactreact/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'components/appBar.js'
+
+const Home = () => {
+    return (
+        <div>
+            <AppBar />
+        </div>
+    );
+};
+
+export default Home;
